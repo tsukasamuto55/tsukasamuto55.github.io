da884e0d6e84537a4c8776c2fb3971cc2d7a4bb3
tsukasamuto55 <tsukasa.muto@live.com>
2021-04-21 20:11:00 -0600

    date modified

--- js/currentDate.js
+++ js/currentDate.js
@@ -11,4 +11,8 @@
 // document.querySelector("span").textContent = lastModified;
 
 const lastModif = document.lastModified;
-document.querySelector("span").textContent = lastModif;
+document.querySelector("#currentDate").textContent = lastModif;
+
+let date = new Date();
+const currnetYear = date.getFullYear();
+document.querySelector("#currentYear").textContent = currnetYear;
