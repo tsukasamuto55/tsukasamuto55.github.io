c5f7ccbe0716ebe5908d9f537192ea6f9b87b06d
tsukasamuto55 <tsukasa.muto@live.com>
2021-05-14 11:27:39 -0600

    minor change

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,36 +1,38 @@
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
 const date = new Date();
 const currentYear = date.getFullYear();
 document.querySelector("#currentYear").textContent = currentYear;
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
+document.querySelector("#currentDate").textContent = fulldate;
 
-const currentDay = days[date.getDay()];
-const currentMonth = months[date.getMonth()];
-const today = `${currentDay}, ${date.getDate()} ${currentMonth} ${currentYear}`;
-document.querySelector("#currentDate").textContent = today;
+// Another way to format date, but this is long.
+// const days = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+
+// const months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+
+// const currentDay = days[date.getDay()];
+// const currentMonth = months[date.getMonth()];
+// const today = `${currentDay}, ${date.getDate()} ${currentMonth} ${currentYear}`;
 
 // One way to make a hamburger menu button work//
 // function toggleMenu() {
