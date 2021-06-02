e474d5d4ec79a3b746e739d68be7820d11ccacbd
tsukasamuto55 <tsukasa.muto@live.com>
2021-06-02 15:40:17 -0600

    Week 7

---
+++ lesson7/script/lazyload.js
@@ -0,0 +1,33 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px",
+};
+
+const loadImages = (Image) => {
+  Image.setAttribute("src", Image.getAttribute("data-src"));
+  Image.onload = () => {
+    Image.removeAttribute("data-src");
+  };
+};
+
+// Check if ItersectionOvserver is supported //
+if ("IntersectionObserver" in window) {
+  const imgObserver = new IntersectionObserver((items, imgObserver) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        imgObserver.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
