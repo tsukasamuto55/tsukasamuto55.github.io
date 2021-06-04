e43a27cfe4d9da68fbdd473fa63b350aeb320c87
tsukasamuto55 <tsukasa.muto@live.com>
2021-06-04 09:52:45 -0600

    week 7 assignment

--- lesson7/script/script.js
+++ lesson7/script/script.js
@@ -2,7 +2,9 @@ const date = new Date();
 const currentYear = date.getFullYear();
 document.querySelector("#currentYear").textContent = currentYear;
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
-document.querySelector("#currentDate").textContent = fulldate;
+
+const today = document.querySelector("#currentDate");
+today.textContent = fulldate;
 
 // Another way to format date, but this is long.
 // const days = [
@@ -39,6 +41,7 @@ document.querySelector("#currentDate").textContent = fulldate;
 //   document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 // }
 
+// Toggle between open button and close button
 let navBar = document.querySelector(".navigation");
 let menuBtn = document.querySelector(".menu");
 let closeBtn = document.querySelector("#closeBtn");
@@ -51,7 +54,42 @@ closeBtn.addEventListener("click", () => {
   navBar.classList.remove("responsive");
 });
 
+// Showing a banner on Friday only
 let banner = document.querySelector(".banner");
 document.addEventListener("DOMContentLoaded", () => {
   if (date.getDay() === 5) banner.style.display = "block";
 });
+
+// Check if a value of local storage I want to use exits or not.
+if (!localStorage.getItem("currentDate")) {
+  // Store a data of when a user visits this site
+  localStorage.setItem("currentDate", fulldate);
+}
+
+let storedDate = localStorage.getItem("currentDate");
+
+// Get the number of days between two dates //
+function getNumberOfDays(first, second) {
+  const date1 = new Date(first);
+  const date2 = new Date(second);
+  // one day in milliseconds
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  // Calculating the time difference between two dates
+  const diffInTime = date2.getTime() - date1.getTime();
+
+  //  Calculating the no. of days between two dates
+  const diffInDays = Math.round(diffInTime / oneDay);
+
+  return diffInDays;
+}
+
+let diffNumberOfDays = getNumberOfDays(storedDate, fulldate);
+let showNumberOfDays = document.getElementById("different-days");
+console.log(diffNumberOfDays);
+
+if (diffNumberOfDays === 1) {
+  showNumberOfDays.textContent = `(You first visited this site on ${storedDate} which was ${diffNumberOfDays} day ago.)`;
+} else {
+  showNumberOfDays.textContent = `(You first visited this site on ${storedDate} which was ${diffNumberOfDays} days ago.)`;
+}
