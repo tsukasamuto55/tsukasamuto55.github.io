91da5dd8792d7091a09c367c26b56bfbdf64075d
tsukasamuto55 <tsukasa.muto@live.com>
2021-06-05 12:37:40 -0600

    .

--- lesson7/script/lazyload.js
+++ lesson7/script/lazyload.js
@@ -5,10 +5,10 @@ const imgOptions = {
   rootMargin: "0px 0px 50px 0px",
 };
 
-const loadImages = (Image) => {
-  Image.setAttribute("src", Image.getAttribute("data-src"));
-  Image.onload = () => {
-    Image.removeAttribute("data-src");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
   };
 };
 
