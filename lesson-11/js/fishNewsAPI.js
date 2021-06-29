50fb392d9d29897e61019f27a3315a826a13d14e
tsukasamuto55 <tsukasa.muto@live.com>
2021-06-28 21:07:07 -0600

    .

--- lesson-11/js/fishNewsAPI.js
+++ lesson-11/js/fishNewsAPI.js
@@ -2,40 +2,42 @@
 
 // const url = `https://newsapi.org/v2/everything?q=idaho+("fish"+"haven")&sortBy=popularity&apiKey=${newsapiKey}`;
 
-const newsapiKey = "a5b701a1f2abe183e1483766dc8465d2";
-const url = `https://gnews.io/api/v4/search?q=example&token=${newsapiKey}`;
+const newsapiKey = "80786e58dc1c8cb0cc8f96aed97419fd";
+const url = `https://gnews.io/api/v4/search?q=idaho&token=${newsapiKey}`;
 
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    const articleInfo = data.articles[0];
+    for (i = 0; i < 2; i++) {
+      const articleInfo = data.articles[i];
 
-    const articleContainer = document.querySelector(".article-container");
+      const articleContainer = document.querySelector(".article-container");
 
-    const articleBox = document.createElement("article");
-    articleBox.classList.add("article-box", "clearfix");
+      const articleBox = document.createElement("article");
+      articleBox.classList.add("article-box", "clearfix");
 
-    const image = document.createElement("img");
-    image.setAttribute("class", "article-img");
-    image.setAttribute("src", articleInfo.image);
-    image.setAttribute("alt", articleInfo.description);
+      const image = document.createElement("img");
+      image.setAttribute("class", "article-img");
+      image.setAttribute("src", articleInfo.image);
+      image.setAttribute("alt", articleInfo.description);
 
-    const title = document.createElement("h3");
-    title.setAttribute("class", "title");
-    title.textContent = articleInfo.title;
+      const title = document.createElement("h3");
+      title.setAttribute("class", "title");
+      title.textContent = articleInfo.title;
 
-    const author = document.createElement("h4");
-    author.classList.add("author");
-    author.textContent = "By " + articleInfo.author;
+      const author = document.createElement("h4");
+      author.classList.add("author");
+      author.textContent = "By " + articleInfo.author;
 
-    const articleContent = document.createElement("p");
-    articleContent.classList.add("headline");
-    articleContent.innerHTML = `${articleInfo.description} <a href='${articleInfo.url}' target='_blank'>...<br>click here to read the full story.</a>`;
+      const articleContent = document.createElement("p");
+      articleContent.classList.add("headline");
+      articleContent.innerHTML = `${articleInfo.description} <a href='${articleInfo.url}' target='_blank'>...<br>click here to read the full story.</a>`;
 
-    articleBox.appendChild(image);
-    articleBox.appendChild(title);
-    articleBox.appendChild(author);
-    articleBox.appendChild(articleContent);
+      articleBox.appendChild(image);
+      articleBox.appendChild(title);
+      articleBox.appendChild(author);
+      articleBox.appendChild(articleContent);
 
-    articleContainer.appendChild(articleBox);
+      articleContainer.appendChild(articleBox);
+    }
   });
