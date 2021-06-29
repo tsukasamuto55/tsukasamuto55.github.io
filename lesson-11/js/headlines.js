0b912a11b0d684f9cb33e0fdc5b2ee9d3980448f
tsukasamuto55 <tsukasa.muto@live.com>
2021-06-28 23:50:48 -0600

    week 11 activity

--- lesson-11/js/headlines.js
+++ lesson-11/js/headlines.js
@@ -1,37 +1,32 @@
 const newsapiKey = "80786e58dc1c8cb0cc8f96aed97419fd";
-const url = `https://gnews.io/api/v4/search?q=idaho&token=${newsapiKey}`;
+const url = `https://gnews.io/api/v4/search?q=idaho&from=2021-06-01&token=${newsapiKey}`;
 
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    for (i = 0; i < 2; i++) {
-      const articleInfo = data.articles[i];
+    for (i = 0; i < 5; i++) {
+      const articleInfo = data.articles[i + 1];
 
-      const articleContainer = document.querySelector(".article-container");
+      const articleContainer = document.querySelector(".home-article-container");
 
       const articleBox = document.createElement("article");
-      articleBox.classList.add("article-box", "clearfix");
+      articleBox.classList.add("home-article-box", "clearfix");
 
       const image = document.createElement("img");
-      image.setAttribute("class", "article-img");
+      image.setAttribute("class", "home-article-img");
       image.setAttribute("src", articleInfo.image);
       image.setAttribute("alt", articleInfo.description);
 
       const title = document.createElement("h3");
-      title.setAttribute("class", "title");
+      title.setAttribute("class", "home-article-title");
       title.textContent = articleInfo.title;
 
-      const author = document.createElement("h4");
-      author.classList.add("author");
-      author.textContent = "By " + articleInfo.author;
-
       const articleContent = document.createElement("p");
-      articleContent.classList.add("headline");
+      articleContent.classList.add("home-headline");
       articleContent.innerHTML = `${articleInfo.description} <a href='${articleInfo.url}' target='_blank'>...<br>click here to read the full story.</a>`;
 
       articleBox.appendChild(image);
       articleBox.appendChild(title);
-      articleBox.appendChild(author);
       articleBox.appendChild(articleContent);
 
       articleContainer.appendChild(articleBox);
