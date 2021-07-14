5e5eca90e8e6bd70fcd02bc486d5c39fe0aa3930
tsukasamuto55 <tsukasa.muto@live.com>
2021-07-13 21:06:11 -0600

    JSON file updated

--- final/js/directory.js
+++ final/js/directory.js
@@ -5,14 +5,39 @@ fetch(directoryURL)
   .then((data) => {
     const directory = data["directories"];
 
-    console.log(directory);
     for (ele of directory) {
-      const card = document.getElementById("test");
+      const card = document.createElement("div");
+      card.classList.add("directory-card");
       const image = document.createElement("img");
       image.setAttribute("src", `${ele.logo}`);
+      image.setAttribute("alt", `${ele.name}`);
       image.setAttribute("width", `60%`);
       image.setAttribute("height", `auto`);
 
       card.appendChild(image);
+
+      const name = document.createElement("h3");
+      name.classList.add("directory-name");
+      name.textContent = ele.name;
+      card.appendChild(name);
+
+      const address = document.createElement("p");
+      address.classList.add("directory-address");
+      address.textContent = ele.address;
+      card.appendChild(address);
+
+      const phone = document.createElement("p");
+      phone.classList.add("directory-phone");
+      phone.textContent = ele.phone;
+      card.appendChild(phone);
+
+      const description = document.createElement("p");
+      description.classList.add("directory-name");
+      description.textContent = ele.description;
+      card.appendChild(description);
+
+      const directoryContainer = document.querySelector(".directory-card-container");
+
+      directoryContainer.appendChild(card);
     }
   });
