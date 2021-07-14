8366ee8e1023d719462eca7958fdf5d7c3b16f4b
tsukasamuto55 <tsukasa.muto@live.com>
2021-07-14 16:51:46 -0600

    JSNO file updated

--- final/js/directory.js
+++ final/js/directory.js
@@ -8,18 +8,29 @@ fetch(directoryURL)
     for (ele of directory) {
       const card = document.createElement("div");
       card.classList.add("directory-card");
+
+      const link = document.createElement("a");
+      link.setAttribute("href", `${ele.website}`);
+
       const image = document.createElement("img");
       image.setAttribute("src", `${ele.logo}`);
       image.setAttribute("alt", `${ele.name}`);
-      image.setAttribute("width", `60%`);
-      image.setAttribute("height", `auto`);
+      image.setAttribute("class", `business-icon`);
+      image.setAttribute("width", `150`);
+      image.setAttribute("height", `150`);
 
-      card.appendChild(image);
+      link.appendChild(image);
+      card.appendChild(link);
 
       const name = document.createElement("h3");
       name.classList.add("directory-name");
       name.textContent = ele.name;
       card.appendChild(name);
+
+      const url = document.createElement("p");
+      url.classList.add("directory-url");
+      url.textContent = ele.website;
+      link.appendChild(url);
 
       const address = document.createElement("p");
       address.classList.add("directory-address");
@@ -32,12 +43,32 @@ fetch(directoryURL)
       card.appendChild(phone);
 
       const description = document.createElement("p");
-      description.classList.add("directory-name");
+      description.classList.add("directory-description");
       description.textContent = ele.description;
       card.appendChild(description);
 
       const directoryContainer = document.querySelector(".directory-card-container");
 
       directoryContainer.appendChild(card);
+    }
+
+    const gridBtn = document.getElementById("grid-icon");
+    const listBtn = document.getElementById("list-icon");
+    const directoryCard = document.querySelectorAll(".directory-card");
+    const directoryCardContainer = document.querySelector(".directory-card-container");
+    const businessIcons = document.querySelectorAll(".business-icon");
+
+    for (let i = 0; i < businessIcons.length; i++) {
+      gridBtn.addEventListener("click", () => {
+        directoryCardContainer.classList.remove("list-card-container");
+        directoryCard[i].classList.remove("list-card");
+        businessIcons[i].style.display = "inline";
+      });
+
+      listBtn.addEventListener("click", () => {
+        directoryCardContainer.classList.add("list-card-container");
+        directoryCard[i].classList.add("list-card");
+        businessIcons[i].style.display = "none";
+      });
     }
   });
