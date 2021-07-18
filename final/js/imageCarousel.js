0e673893261a7d3d14ba4081dd089add9e76f310
tsukasamuto55 <tsukasa.muto@live.com>
2021-07-18 03:51:10 -0600

    city page updated

---
+++ final/js/imageCarousel.js
@@ -0,0 +1,39 @@
+const imgs = document.getElementById("imgs");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const img = document.querySelectorAll(".image-card img");
+
+let idx = 0;
+
+let interval = setInterval(autoSlide, 2500);
+
+function autoSlide() {
+  idx++;
+  slideImage();
+}
+
+function slideImage() {
+  if (idx > img.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = img.length - 1;
+  }
+  imgs.style.transform = `translateX(${-idx * 600}px)`;
+}
+
+function cancelInterval() {
+  clearInterval(interval);
+  interval = setInterval(autoSlide, 2500);
+}
+
+nextBtn.addEventListener("click", () => {
+  idx++;
+  slideImage();
+  cancelInterval();
+});
+
+prevBtn.addEventListener("click", () => {
+  idx--;
+  slideImage();
+  cancelInterval();
+});
