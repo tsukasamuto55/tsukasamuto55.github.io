734432ff2c36660d2bd03f7e84164d2450140ed8
tsukasamuto55 <tsukasa.muto@live.com>
2021-07-19 19:59:18 -0600

    Home and City page updated

---
+++ final/js/videoCarousel.js
@@ -0,0 +1,25 @@
+const imgs = document.getElementById("imgs");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const img = document.querySelectorAll(".image-card img");
+
+let idx = 0;
+
+function slideImage() {
+  if (idx > img.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = img.length - 1;
+  }
+  imgs.style.transform = `translateX(${-idx * 100}%)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  idx++;
+  slideImage();
+});
+
+prevBtn.addEventListener("click", () => {
+  idx--;
+  slideImage();
+});
