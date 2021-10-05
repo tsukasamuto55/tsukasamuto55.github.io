7a3ecf5d86d99e8bf885f9c19899078326da4dbc
tsukasamuto55 <tsukasa.muto@live.com>
2021-10-04 23:12:41 -0600

    footer of index page is updated.

--- final/js/currentDate.js
+++ final/js/currentDate.js
@@ -1,6 +1,3 @@
-const lastModif = document.lastModified;
-document.querySelector("#currentDate").textContent = lastModif;
-
 const date = new Date();
 const currentYear = date.getFullYear();
 document.querySelector("#currentYear").textContent = currentYear;
